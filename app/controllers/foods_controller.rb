c807badda6829ac1b93b177b08672b86731fb2bf
kohei <shoot0321@gmail.com>
2021-06-25 10:05:13 +0900

    foodsコントローラのリファクタリングを実施

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,5 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only: %i[edit update destroy]
   def index
     @foods = Food.includes(:user).order(:created_at)
   end
@@ -16,20 +17,14 @@ class FoodsController < ApplicationController
     @food = Food.find(params[:id])
   end
 
-  def edit
-    @food = Food.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @food = current_user.foods.find_by(id: params[:id])
-    redirect_to root_path, alert: "権限がありません" if @food.nil?
     @food.update!(food_params)
     redirect_to @food
   end
 
   def destroy
-    @food = current_user.foods.find_by(id: params[:id])
-    redirect_to root_path, alert: "権限がありません" if @food.nil?
     @food.destroy!
     redirect_to @food
   end
@@ -38,5 +33,10 @@ class FoodsController < ApplicationController
 
   def food_params
     params.require(:food).permit(:name, :comment)
+  end
+
+  def set_food
+    @food = current_user.foods.find_by(id: params[:id])
+    redirect_to root_path, alert: "権限がありません" if @food.nil?
   end
 end
