c253f27e15cf898424c9772d19087816cd6182b5
kohei <shoot0321@gmail.com>
2021-06-28 12:22:29 +0900

    likesコントローラのアクションを定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,11 @@
 class LikesController < ApplicationController
+  def create
+    current_user.likes.create!(post_id: params[:post_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.likes.find_bt(post_id: params[:post_id]).destroy!
+    redirect_back(fallback_location: root_path)
+  end
 end
